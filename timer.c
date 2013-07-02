6c183be26a2a87c51c31a017f10223f737932b72
cuki <mauricio@krebs.com.br>
2013-07-02 10:31:00 -0300

    commit 1

--- timer.c
+++ timer.c
@@ -1,35 +1,22 @@
-#include<18F452.h>
+#include<18F252.h>
 
 #fuses HS,NOWDT,NOLVP,BROWNOUT,PUT
 
-#use delay(clock=8000000)
-//#use delay(clock=32MHz, crystal=8MHz)
-//#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
+//#use delay(clock=25MHz)
+//#use rs232(baud=9600, xmit=PIN_C6)
 
-/*#INT_TIMER0
- void isr_timer0() {
- clear_interrupt(int_timer0);
- set_timer0(0);
- output_toggle(pin_b5);
- }*/
+#INT_TIMER0
+void isr_timer0() {
+	clear_interrupt(INT_TIMER0);
+	output_toggle(PIN_C3);
+}
 
 int main(void) {
-
-	/*	setup_timer_0(t0_internal | t0_div_1);
-	 set_timer0(0);
-	 clear_interrupt(int_timer0);
-
-	 enable_interrupts(int_timer0);
-	 enable_interrupts(global);
-
-	 output_high(pin_b4);*/
-
-	while (true) {
-		output_toggle(pin_b5);
-		/*output_high(pin_b5);
-		 while (get_timer0())
-		 ;
-		 output_low(pin_b5);*/
-	}
+	clear_interrupt(INT_TIMER0);
+	setup_timer_0(T0_DIV_1);
+	enable_interrupts(INT_TIMER0);
+	enable_interrupts(GLOBAL);
+	while (TRUE)
+		;
 	return 0;
 }
