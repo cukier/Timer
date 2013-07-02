eae25015c9f78d32daf01d4af9cd97533ba192d7
cuki <mauricio@krebs.com.br>
2013-07-02 10:57:03 -0300

    18F252 timer2 disparando a 1ms (8Mhz @ 32MHz)

--- timer.c
+++ timer.c
@@ -1,20 +1,20 @@
 #include<18F252.h>
 
-#fuses HS,NOWDT,NOLVP,BROWNOUT,PUT
+#fuses H4
 
-//#use delay(clock=25MHz)
+#use delay(clock=32MHz, crystal=8MHz)
 //#use rs232(baud=9600, xmit=PIN_C6)
 
-#INT_TIMER0
+#INT_TIMER2
 void isr_timer0() {
-	clear_interrupt(INT_TIMER0);
+	clear_interrupt(INT_TIMER2);
 	output_toggle(PIN_C3);
 }
 
 int main(void) {
-	clear_interrupt(INT_TIMER0);
-	setup_timer_0(T0_DIV_1);
-	enable_interrupts(INT_TIMER0);
+	clear_interrupt(INT_TIMER2);
+	setup_timer_2(T2_DIV_BY_16, 0xFA, 2);
+	enable_interrupts(INT_TIMER2);
 	enable_interrupts(GLOBAL);
 	while (TRUE)
 		;
