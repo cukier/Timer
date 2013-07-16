e1d41e7dd76223732c50435a30db8cfbf26be18d
cuki <mauricio@krebs.com.br>
2013-07-16 10:52:49 -0300

    timer0 @ 8MHz para estourar a cada 6.537s

--- timer.c
+++ timer.c
@@ -1,22 +1,30 @@
 #include<18F252.h>
 
-#fuses H4
+#fuses HS
 
-#use delay(clock=32MHz, crystal=8MHz)
+#use delay(clock=8MHz)
 //#use rs232(baud=9600, xmit=PIN_C6)
 
-#INT_TIMER2
+#define tempo 14465
+
+static long registrador;
+
+#INT_TIMER0
 void isr_timer0() {
-	clear_interrupt(INT_TIMER2);
+	clear_interrupt(INT_TIMER0);
+	set_timer0(tempo);
 	output_toggle(PIN_C3);
 }
 
 int main(void) {
-	clear_interrupt(INT_TIMER2);
-	setup_timer_2(T2_DIV_BY_16, 0xFA, 2);
-	enable_interrupts(INT_TIMER2);
+	clear_interrupt(INT_TIMER0);
+	setup_timer_0(T0_DIV_256);
+	set_timer0(tempo);
+	enable_interrupts(INT_TIMER0);
 	enable_interrupts(GLOBAL);
-	while (TRUE)
-		;
+	while (TRUE){
+		if (registrador != get_timer0())
+			registrador = get_timer0();
+	}
 	return 0;
 }
