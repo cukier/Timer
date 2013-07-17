aa007f76db4cfc976f5d03d518fb3d33c01e4f7b
cuki <mauricio@krebs.com.br>
2013-07-17 09:20:30 -0300

    timer2 @ 8MHz estourando 0.01s

--- timer.c
+++ timer.c
@@ -5,26 +5,18 @@
 #use delay(clock=8MHz)
 //#use rs232(baud=9600, xmit=PIN_C6)
 
-#define tempo 14465
-
-static long registrador;
-
-#INT_TIMER0
-void isr_timer0() {
-	clear_interrupt(INT_TIMER0);
-	set_timer0(tempo);
+#INT_TIMER2
+void isr_timer2() {
+	clear_interrupt(INT_TIMER2);
 	output_toggle(PIN_C3);
 }
 
 int main(void) {
-	clear_interrupt(INT_TIMER0);
-	setup_timer_0(T0_DIV_256);
-	set_timer0(tempo);
-	enable_interrupts(INT_TIMER0);
+	clear_interrupt(INT_TIMER2);
+	setup_timer_2(T2_DIV_BY_16, 77, 16);
+	enable_interrupts(INT_TIMER2);
 	enable_interrupts(GLOBAL);
-	while (TRUE){
-		if (registrador != get_timer0())
-			registrador = get_timer0();
-	}
+	while (TRUE)
+		;
 	return 0;
 }
