43986551001e5293f656d850435b9ec9a6868ad5
cuki <mauricio@krebs.com.br>
2013-08-01 10:25:48 -0300

    Uso do pino T0CKI(PIN_A4) como contador do timer0 para 18F45K20

--- timer.c
+++ timer.c
@@ -1,22 +1,21 @@
-#include<18F252.h>
+/*Uso do pino T0CKI(PIN_A4) como contador do timer0 para 18F45K20*/
 
-#fuses HS
+#include<18F45K20.h>
 
-#use delay(clock=8MHz)
-//#use rs232(baud=9600, xmit=PIN_C6)
+#FUSES HS
+#use delay(clock=15MHz)
+#use rs232(xmit=PIN_C6, baud=9600)
 
-#INT_TIMER2
-void isr_timer2() {
-	clear_interrupt(INT_TIMER2);
-	output_toggle(PIN_C3);
-}
+long aux, cont;
 
 int main(void) {
-	clear_interrupt(INT_TIMER2);
-	setup_timer_2(T2_DIV_BY_16, 77, 16);
-	enable_interrupts(INT_TIMER2);
-	enable_interrupts(GLOBAL);
-	while (TRUE)
-		;
-	return 0;
+	set_timer0(0);
+	setup_timer_0(T0_EXT_H_TO_L);
+	while (TRUE) {
+		cont = get_timer0();
+		if (aux != cont) {
+			aux = cont;
+			printf("\f%Lu", cont);
+		}
+	}
 }
