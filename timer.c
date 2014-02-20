725d9dbc39640fc8fc2729f5d6b7569f3ab2ec5e
cuki <mauricio@krebs.com.br>
2014-02-20 10:58:23 -0300

    ok

--- timer.c
+++ timer.c
@@ -1,21 +1,29 @@
-/*Uso do pino T0CKI(PIN_A4) como contador do timer0 para 18F45K20*/
+/*Uso do timer 0 com fonte externa no 18F542 (pin_a4)*/
 
-#include<18F45K20.h>
+#include<18F452.h>
 
 #FUSES HS
-#use delay(clock=15MHz)
+
+#use delay(clock=8MHz)
 #use rs232(xmit=PIN_C6, baud=9600)
 
-long aux, cont;
+#INT_TIMER0
+void isr_timer0() {
+	clear_interrupt(INT_TIMER0);
+	set_timer0(252);
+	output_toggle(PIN_C3);
+}
 
 int main(void) {
-	set_timer0(0);
-	setup_timer_0(T0_EXT_H_TO_L);
+
+	setup_timer_0(T0_DIV_1 | T0_EXT_L_TO_H | T0_8_BIT);
+	set_timer0(252);
+	clear_interrupt(INT_TIMER0);
+	enable_interrupts(INT_TIMER0 | GLOBAL);
+
 	while (TRUE) {
-		cont = get_timer0();
-		if (aux != cont) {
-			aux = cont;
-			printf("\f%Lu", cont);
-		}
+		;
 	}
+
+	return 0;
 }
