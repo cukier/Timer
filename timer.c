4fae1fb5dd87fa616882ec814bfd6cdebbd8229d
cuki <mauricio@krebs.com.br>
2014-09-18 16:42:16 -0300

    timer1 no 452 externo

--- timer.c
+++ timer.c
@@ -1,29 +1,25 @@
 /*Uso do timer 0 com fonte externa no 18F542 (pin_a4)*/
 
-#include<18F452.h>
+#include<18F252.h>
 
 #FUSES HS
 
 #use delay(clock=8MHz)
 #use rs232(xmit=PIN_C6, baud=9600)
 
-#INT_TIMER0
-void isr_timer0() {
-	clear_interrupt(INT_TIMER0);
-	set_timer0(252);
-	output_toggle(PIN_C3);
-}
+long t0, t0aux;
 
 int main(void) {
 
-	setup_timer_0(T0_DIV_1 | T0_EXT_L_TO_H | T0_8_BIT);
-	set_timer0(252);
-	clear_interrupt(INT_TIMER0);
-	enable_interrupts(INT_TIMER0 | GLOBAL);
+	setup_timer_1(T1_EXTERNAL | T1_DIV_BY_1);
+	set_timer1(0);
 
 	while (TRUE) {
-		;
+		t0 = get_timer1();
+		if (t0 != t0aux) {
+			t0aux = t0;
+			printf("%ld\n\r", t0);
+		}
 	}
-
 	return 0;
 }
